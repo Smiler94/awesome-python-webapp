552b9c06c2686993666062a1600b33b1e2dabac7
linzhen <273487984@qq.com>
2016-09-08 07:18:50 +0800

    add db module

---
+++ www/static/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2016-08-31 06:56:50
+# @Author  : Lin zhen (273487984@qq.com)
+# @Link    : 
+# @Version : $Id$
+
+from transwarp import db
